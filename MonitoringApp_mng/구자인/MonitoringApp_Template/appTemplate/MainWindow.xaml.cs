848c54a1d65a256654f7b75266f643734ec2b0ac
KOOJAIN <rnwkdls97@naver.com>
2023-07-13 15:00:22 +0900

    7.13

--- MonitoringApp_mng/구자인/MonitoringApp_Template/appTemplate/MainWindow.xaml.cs
+++ MonitoringApp_mng/구자인/MonitoringApp_Template/appTemplate/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace appTemplate
 {
@@ -21,9 +22,41 @@ namespace appTemplate
     /// </summary>
     public partial class MainWindow : MetroWindow   // 솔루션 패키지에서 MahApp.Metro 삭제 후 다시 설치 필수!!! 
     {
+        public ViewModel ViewModel { get; set; }
+        private DispatcherTimer _timer;
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel = new ViewModel();
+            DataContext = ViewModel;
+
+            // 더미 데이터를 업데이트하는 타이머 설정
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(3);
+            _timer.Tick += Timer_Tick;
+            _timer.Tick += Timer_Tick2;
+
+            _timer.Start();
+        }
+        //습도
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // 더미 데이터를 생성하고 ViewModel 속성에 할당
+            Random random = new Random();
+            double dummyValue1 = random.Next(20, 80); // LvcLivingHumid의 더미 데이터
+            double dummyValue2 = random.Next(20, 80); // LvcLivingHumid2의 더미 데이터
+            ViewModel.Value1 = dummyValue1;
+            ViewModel.Value2 = dummyValue2;
+        }
+        //온도
+        private void Timer_Tick2(object sender, EventArgs e)
+        {
+            // 더미 데이터를 생성하고 ViewModel 속성에 할당
+            Random random = new Random();
+            double dummyTempValue = random.Next(20, 30); // LvcTemp의 더미 데이터
+            double dummyTemp2Value = random.Next(20, 30); // LvcTemp2의 더미 데이터
+            ViewModel.TempValue = dummyTempValue;
+            ViewModel.Temp2Value = dummyTemp2Value;
         }
 
     }
---
+++ MonitoringApp_mng/구자인/MonitoringApp_Template/appTemplate/Views/ViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+
+public class ViewModel : INotifyPropertyChanged
+{
+    // 거실  온도
+    private double _tempValue;
+    public double TempValue
+    {
+        get { return _tempValue; }
+        set
+        {
+            if (_tempValue != value)
+            {
+                _tempValue = value;
+                OnPropertyChanged(nameof(TempValue));
+            }
+        }
+    }
+    // 화장실 온도
+    private double _temp2Value;
+    public double Temp2Value
+    {
+        get { return _temp2Value; }
+        set
+        {
+            if (_temp2Value != value)
+            {
+                _temp2Value = value;
+                OnPropertyChanged(nameof(Temp2Value));
+            }
+        }
+    }
+
+    // 거실 습도
+    private double _value1;
+    public double Value1
+    {
+        get { return _value1; }
+        set
+        {
+            if (_value1 != value)
+            {
+                _value1 = value;
+                OnPropertyChanged(nameof(Value1));
+            }
+        }
+    }
+    // 화장실 습도
+    private double _value2;
+    public double Value2
+    {
+        get { return _value2; }
+        set
+        {
+            if (_value2 != value)
+            {
+                _value2 = value;
+                OnPropertyChanged(nameof(Value2));
+            }
+        }
+    }
+
+    // ... 공통부분?,,,
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
