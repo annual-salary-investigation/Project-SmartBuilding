cf49fe70e12b69a0ab0217eebe0f2559385f9e36
LaniJeong <123913969+LaniJeong@users.noreply.github.com>
2023-07-24 16:45:54 +0900

    온,습도 ui

---
+++ MonitoringApp_mng/수민/monitoring_home/Models/Weather.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appTemplate.Models
+{
+    internal class Weather
+    {
+        /* API 검색결과
+         "item": [
+          {
+            "baseDate": "20230712",
+            "baseTime": "1200",
+            "category": "PTY",
+            "nx": 98,
+            "ny": 74,
+            "obsrValue": "1"
+          },
+         */
+
+        public int BaseDate { get; set; }
+        public int BaseTime { get; set; }
+        public string Category { get; set; }
+        public int NX { get; set; }
+        public int NY { get; set; }
+        public double ObsrValue { get; set; }
+    }
+}
